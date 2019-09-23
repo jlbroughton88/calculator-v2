33041c856e6d155239c65ddf26858b506e8bee3c
jlb1999 <jlbroughton88@gmail.com>
2019-09-23 12:08:34 -0400

    removed build from client git ignore

---
+++ client/build/precache-manifest.30865eae9bcbad7b44c0a29ff310c0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754bcf646bf62ce4e70952f71c604441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff3a3ca22f34592fb11",
+    "url": "/static/css/main.71984861.chunk.css"
+  },
+  {
+    "revision": "5851964a1fbd5ae75a20",
+    "url": "/static/js/2.ae7b2b4e.chunk.js"
+  },
+  {
+    "revision": "8ff3a3ca22f34592fb11",
+    "url": "/static/js/main.8a148e4a.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
